2c8236bb62330200afa50927e5e7e66118390910
Vadim Slyvka <vadik-slyka@yandex.ru>
2022-11-14 03:53:21 +0500

    Second commit

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-    echo 'Hello world!';
+    function get_ip()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+        $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    print_r($ip);
+
+}
+
+get_ip();
 
 ?>
